2141d439f9987ebe1d8c112ebb8665a096758ca7
pastelariapastelecana <pastelariapastelecana@gmail.com>
2025-11-14 16:33:28 -0300

    Update supabaseClient.js

--- backend/src/config/supabaseClient.js
+++ backend/src/config/supabaseClient.js
@@ -1,4 +1,4 @@
-const { createClient } = require('@supabase/supabase-js');
+const { createClient } = require('@supabase/supabase-js'); 
 
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
